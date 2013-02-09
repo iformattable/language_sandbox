3c267f202d2d674285b187dcbb0c857c18feed7a
Christopher Atkins <christopher316@gmail.com>
2013-02-08 20:24:07 -0500

    finished DNA

---
+++ DNA.fs
@@ -0,0 +1,11 @@
+#light
+
+let rec count a c g t = function
+  | 'A'::nbs -> count (a+1) c g t nbs
+  | 'C'::nbs -> count a (c+1) g t nbs
+  | 'G'::nbs -> count a c (g+1) t nbs
+  | 'T'::nbs -> count a c g (t+1) nbs
+  | _ -> printfn "%d %d %d %d" a c g t
+
+let (_::s::_) = System.Environment.GetCommandLineArgs() |> Seq.toList
+count 0 0 0 0 (s.ToCharArray() |> Seq.toList)
